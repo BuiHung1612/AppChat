94f144791cb58dcc47d8dee7519b894bc94237ea
BuiHung1612 <hungbt20@gmail.com>
2021-09-26 22:18:55 +0700

    Color sẽ dùng thường xuyên trong app

---
+++ src/themes/Colors.ts
@@ -0,0 +1,20 @@
+
+const Colors = {
+    cyan: "#06b6d4",
+    black50: "rgba(0,0,0,0.5)",
+    black1: "#000000",
+    black2: "#1D1D1D",
+    black3: "#282828",
+    white: "#ffffff",
+    gray1: "#333333",
+    gray2: "#4F4F4F",
+    gray3: "#828282",
+    gray4: "#BDBDBD",
+    gray5: "#E0E0E0",
+    gray6: "#f1f1f1",
+    transparent: "rgba(0,0,0,0)",
+    black_blue: '#000030'
+
+};
+
+export default Colors;
