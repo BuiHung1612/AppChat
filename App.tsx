63809b3d24e7781f03cdce72d2b3f4943a9a5c4a
BuiHung1612 <hungbt20@gmail.com>
2021-09-26 21:21:08 +0700

    setup project

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>xin chao viet nam</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
