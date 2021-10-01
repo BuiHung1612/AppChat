97d188e09f804282e4cf5e0c55c0ef3a67178920
BuiHung1612 <hungbt20@gmail.com>
2021-10-01 20:45:56 +0700

    update main navigator

--- src/navigation/MainNavigator.tsx
+++ src/navigation/MainNavigator.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../modules/home/Home';
-import Ionicons from 'react-native-vector-icons/Ionicons'
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import News from '../modules/news/News';
 import Converstation from '../modules/converstation/Converstation';
 import Profile from '../modules/profile/Profile';
 import { Image } from 'react-native';
-import Icon from '../assets'
+import Icon from '../assets';
 import Fonts from '../themes/Fonts';
 const Tab = createBottomTabNavigator();
 
@@ -14,60 +14,99 @@ const MainNavigator = () => {
     return (
         <Tab.Navigator
             screenOptions={{
-
                 tabBarHideOnKeyboard: true,
                 headerShown: true,
                 // tabBarShowLabel: false,
                 tabBarStyle: [
                     {
-                        display: 'flex',
+                        display: 'flex'
                     },
-                    null,
+                    null
                 ],
-                headerTitleStyle: {
-                    color: '#8726FE',
-                    fontFamily: Fonts.extraBold,
-                    fontSize: 30
+                headerStyle: {
+                    shadowColor: 'transparent'
                 },
-                headerTitleAlign: 'center',
-
+                headerTitleStyle: {
+                    color: 'black',
+                    fontFamily: Fonts.bold,
+                    fontSize: 24,
+                    backgroundColor: 'transparent'
+                },
+                headerTitleAlign: 'center'
             }}
         >
-            <Tab.Screen name="Home" component={Home} options={({ route, navigation }) => {
-                return {
-                    title: "STRANGE",
-                    tabBarIcon: ({ color, size }) => (
-                        <Ionicons name="newspaper-outline" color={color} size={20} />
-                    ),
-                };
-            }} />
-            <Tab.Screen name="News" component={News} options={({ route, navigation }) => {
-                return {
-                    title: 'Bảng tin',
-                    tabBarIcon: ({ color, size }) => (
-                        <Ionicons name="planet-outline" color={color} size={20} />
-                    ),
-                };
-            }} />
-            <Tab.Screen name="Converstation" component={Converstation} options={({ route, navigation }) => {
-                return {
-                    title: 'Trò chuyện',
-                    tabBarIcon: ({ color, size }) => (
-                        <Ionicons name="chatbubbles-outline" color={color} size={20} />
-                    ),
-                };
-            }} />
-            <Tab.Screen name="Me" component={Profile} options={({ route, navigation }) => {
-                return {
-                    title: 'Tôii',
-                    tabBarIcon: ({ color, size }) => (
-                        <Ionicons name="leaf-outline" color={color} size={20} />
-                    ),
-                };
-            }} />
+            <Tab.Screen
+                name="Home"
+                component={Home}
+                options={({ route, navigation }) => {
+                    return {
+                        title: 'Trang chủ',
+                        tabBarIcon: ({ color, size }) => (
+                            <Ionicons
+                                name="newspaper-outline"
+                                color={color}
+                                size={20}
+                            />
+                        )
+                    };
+                }}
+            />
+            <Tab.Screen
+                name="News"
+                component={News}
+                options={({ route, navigation }) => {
+                    return {
+                        title: 'Bảng tin',
+                        tabBarIcon: ({ color, size }) => (
+                            <Ionicons
+                                name="planet-outline"
+                                color={color}
+                                size={20}
+                            />
+                        )
+                    };
+                }}
+            />
+            <Tab.Screen
+                name="Converstation"
+                component={Converstation}
+                options={({ route, navigation }) => {
+                    return {
+                        title: 'Trò chuyện',
+                        tabBarIcon: ({ color, size }) => (
+                            <Ionicons
+                                name="chatbubbles-outline"
+                                color={color}
+                                size={20}
+                            />
+                        ),
+                        headerTitleStyle: {
+                            color: 'black',
+                            fontFamily: Fonts.bold,
+                            fontSize: 22,
+                            backgroundColor: 'transparent'
+                        }
+                    };
+                }}
+            />
+            <Tab.Screen
+                name="Me"
+                component={Profile}
+                options={({ route, navigation }) => {
+                    return {
+                        tabBarIcon: ({ color, size }) => (
+                            <Ionicons
+                                name="leaf-outline"
+                                color={color}
+                                size={20}
+                            />
+                        ),
+                        headerShown: false
+                    };
+                }}
+            />
         </Tab.Navigator>
-    )
-}
+    );
+};
 
-export default MainNavigator
-
+export default MainNavigator;
