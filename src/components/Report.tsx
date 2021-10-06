760f2878a4b4180cac416ef96ad9ec2aed945f65
BuiHung1612 <hungbt20@gmail.com>
2021-10-06 12:51:12 +0700

    add component report

---
+++ src/components/Report.tsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native'
+import { BottomSheet } from 'react-native-elements'
+import Colors from '../themes/Colors'
+import Fonts from '../themes/Fonts'
+
+interface ReportButton {
+    isVisible: boolean,
+    button1: string,
+    button2: string,
+    cancelLabel: string,
+    setVisible: (isVisible: boolean) => void,
+}
+
+const Report = ({ isVisible, button1, button2, cancelLabel, setVisible }: ReportButton) => {
+
+
+    return (
+        <BottomSheet
+
+            isVisible={isVisible}
+            modalProps={{ animationType: 'none', }}
+            containerStyle={{ backgroundColor: 'rgba(0.5, 0.25, 0, 0.2)' }}
+        >
+            <TouchableOpacity onPress={() => setVisible(false)} style={{ backgroundColor: 'red', flex: 1 }}>
+                <TouchableOpacity style={styles.reportButton}>
+                    <Text style={styles.reportLabel}>{button1}</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={[styles.reportButton, { backgroundColor: '#FF68A3' }]}>
+                    <Text style={styles.reportLabel}>{button2}</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.cancelButton}
+                    onPress={() => setVisible(false)}
+                >
+                    <Text style={styles.cancelLabel}>{cancelLabel}</Text>
+                </TouchableOpacity>
+            </TouchableOpacity>
+        </BottomSheet>
+    )
+}
+
+export default Report
+
+const styles = StyleSheet.create({
+
+    reportButton: {
+        marginHorizontal: 20,
+        borderRadius: 10,
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#8726FE',
+        marginBottom: 10,
+    },
+    reportLabel: {
+        fontFamily: Fonts.bold,
+        fontSize: 16,
+        color: Colors.white
+    },
+    cancelButton: {
+        marginHorizontal: 20,
+        borderRadius: 10,
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        marginBottom: 20
+    },
+    cancelLabel: {
+        fontSize: 16,
+        color: Colors.black1,
+        fontFamily: Fonts.medium
+    }
+})
