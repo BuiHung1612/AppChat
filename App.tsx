3dee984621d65981e4e0736b471b75490939f50c
BuiHung1612 <hungbt20@gmail.com>
2021-10-07 22:04:33 +0700

    Thêm redux và redux thunk vào project

--- App.tsx
+++ App.tsx
@@ -1,12 +1,61 @@
+// import React from 'react'
+// import { StyleSheet, Text, View } from 'react-native'
+// import AppNavigator from './src/navigation/AppNavigator'
+// import { createStore, applyMiddleware } from 'redux';
+// import { Provider } from 'react-redux';
+// import thunk from 'redux-thunk';
+// import rootReducer from './src/shared/redux/index';
+// import AsyncStorage from '@react-native-async-storage/async-storage';
+// import { persistStore, persistReducer } from 'redux-persist';
+// import { PersistGate } from 'redux-persist/es/integration/react';
+
+
+// const persistConfig = {
+//   key: 'root',
+//   storage: AsyncStorage,
+// };
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
+// let store = createStore(persistedReducer, applyMiddleware(thunk));
+// let persistor = persistStore(store);
+// const App = () => {
+//   return (
+//     <Provider store={store}>
+//       <PersistGate loading={null} persistor={persistor}>
+//         <AppNavigator />
+//       </PersistGate>
+//     </Provider>
+//   );
+// };
+
+
+// export default App
+
+// const styles = StyleSheet.create({})
+
+
+
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import AppNavigator from './src/navigation/AppNavigator'
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './src/shared/redux/index';
+
+
+
+
+let store = createStore(rootReducer, applyMiddleware(thunk));
 
 const App = () => {
   return (
-    <AppNavigator />
-  )
-}
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
+  );
+};
+
 
 export default App
 
---
+++ src/shared/redux/index.ts
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+import ProfileReducer from '../../modules/profile/ProfileReducer';
+const rootReducer = combineReducers({
+    ProfileReducer,
+
+});
+export default rootReducer;
