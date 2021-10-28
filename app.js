876dcfaf936f4e97c60f77ca369242e6ca234139
BuiHung1612 <hungbt20@gmail.com>
2021-10-28 18:42:18 +0700

    thêm post

--- app.js
+++ app.js
@@ -3,34 +3,27 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const cors = require('cors');
-
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users/users');
+var postsRouter = require('./routes/posts/Posts');
 
 var app = express();
-
+var cors = require('cors');
+app.use(cors());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({ origin: true, credentials: true }));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/users/posts', postsRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
