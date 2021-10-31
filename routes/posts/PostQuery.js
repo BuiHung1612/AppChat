0c252be20e5da8390e95e85413c66ca4d08b3e63
BuiHung1612 <hungbt20@gmail.com>
2021-10-31 22:16:51 +0700

    post bài và get list bài đăng

--- routes/posts/PostQuery.js
+++ routes/posts/PostQuery.js
@@ -3,19 +3,61 @@ const sql = require("mssql");
 const jwt = require("jsonwebtoken")
 var crypto = require("crypto");
 
-
-async function getPosts() {
+async function listPost(userId) {
     try {
         let pool = await sql.connect(config);
-        let res = await pool.request().query("SELECT *  FROM users");
-        return res.recordsets;
+        let listPost = await pool.request().query(`SELECT *  FROM posts order by create_up`);
+        let listUser = await pool.request().query(`SELECT *  FROM users`);
+        let newarr = listPost.recordset.map(e => ({
+            ...e, user_name: listUser.recordset.find(a => a.user_id == e.user_id).user_name,
+            user_image: listUser.recordset.find(a => a.user_id == e.user_id).user_image,
+            sex: listUser.recordset.find(a => a.user_id == e.user_id).sex,
+            age: listUser.recordset.find(a => a.user_id == e.user_id).age,
+
+        }))
+
+        let checkme = []
+        checkme.push(newarr[0])
+        newarr.map(e => checkme.find(a => {
+            if (a.user_id != e.user_id) {
+                checkme.push(e)
+            }
+        }))
+        // console.log('newarr', newarr);
+        return checkme;
     } catch (error) {
         console.log(" mathus-error :" + error);
     }
 }
 
+async function getPosts(userId) {
+    try {
+        let pool = await sql.connect(config);
+        let res = await pool.request().query(`SELECT *  FROM posts where user_id='${userId}' order by create_up`);
+        return res.recordset;
+    } catch (error) {
+        console.log(" mathus-error :" + error);
+    }
+}
+
+
+async function CreatePostFromToken(userId, postContent, imagePost) {
+    try {
+        var postId = crypto.randomBytes(20).toString('hex');
+        var commentId = userId.slice(0, 10)
+        var time = new Date().toLocaleDateString()
+        let pool = await sql.connect(config);
+        let res = await pool.request().query(`INSERT INTO posts(post_id,user_id,create_up,like_number,post_content,comment_id,post_image) VALUES ('${postId}','${userId}','${time}',0,'${postContent}','${commentId}','${imagePost}')`);
+        // return res.recordsets;
+        return 'CREATE_SUCCESS'
+
+    } catch (error) {
+        console.log(" mathus-error :" + error);
+    }
+}
 module.exports = {
     getPosts: getPosts,
-
+    CreatePostFromToken: CreatePostFromToken,
+    listPost: listPost
 }
 
--- routes/posts/Posts.js
+++ routes/posts/Posts.js
@@ -4,13 +4,88 @@ var router = express.Router();
 const sql = require("./PostQuery");
 const jwt = require('jsonwebtoken')
 
+const verifyToken = (req, res, next) => {
+    const bearerHeader = req.headers['authorization'];
+    if (typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(' ')
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next()
+    }
+    else {
+        res.sendStatus(403)
+    }
+}
+router.post('/create', verifyToken, (req, res, next) => {
+    jwt.verify(req.token, 'secretKey', (err, authData) => {
+        if (err) {
+            res.sendStatus(403)
+        }
+        else {
 
-router.get("/posts", function (req, res, next) {
-    sql.getPosts().then((result) => {
+            let userId = authData.user.user.recordset[0].user_id
+            let postContent = req.body.postContent
+            let imagePost = req.body.imagePost
+            sql.CreatePostFromToken(userId, postContent, imagePost).then((result) => {
+                res.json({
+                    message: 'Tạo bài viết thành công',
+                    status: 200,
+                    error: null
+                })
+            })
+        }
+    })
+})
 
-        res.json(result[0]);
+const TokenVerify = (req, res, next) => {
+    const bearerHeader = req.headers['authorization'];
+    if (typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(' ')
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next()
+    }
+    else {
+        res.sendStatus(403)
+    }
+}
+
+
+router.get("/list", function (req, res, next) {
+
+    sql.listPost().then((result) => {
+        return res.json({ message: 'thành công', status: 200, error: null, data: result });
+
+    }).catch(err => res.json({ message: 'Không có kết quả trả về' }))
+});
+
+
+router.post("/getPostFromUserId", function (req, res, next) {
+    let userId = req.body.userId
+    sql.getPosts(userId).then((result) => {
+        return res.json({ message: 'thành công', status: 200, error: null, data: result });
 
     }).catch(err => res.json({ message: 'Không có của trả về' }))
+});
+
+router.post("/getListPost", TokenVerify, function (req, res, next) {
+    jwt.verify(req.token, 'secretKey', (err, authData) => {
+        if (err) {
+            res.sendStatus(403)
+        }
+        else {
+            let userId = authData.user.user.recordset[0].user_id
+            sql.getPosts(userId).then((result) => {
+
+                res.json({
+                    message: 'Thông tin Cá nhân',
+                    list_Post: result,
+
+                })
+            })
+
+        }
+    })
 });
 
 
